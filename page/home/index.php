4168fa91a0d43d5519e2891ffec2edcd8e0f9050
Buenaventura123 <202310536@fit.edu.ph>
2025-05-24 10:17:37 +0800

    feat: created home page

---
+++ page/home/index.php
@@ -0,0 +1,27 @@
+<?php
+$pageTitle = 'Home';
+$pageCss = '/AD-Bookstore/page/home/assets/css/home.css';
+require_once __DIR__ . '/../../components/componentGroup/header.component.php';
+require_once __DIR__ . '/../../utils/app_utils.php';
+
+// Get all books using the utility function
+$books = getAllBooks();
+?>
+
+<section class="book-list">
+    <h2>Our Books</h2>
+    <div class="books-grid">
+        <?php foreach ($books as $book): ?>
+            <?php
+            // Pass the current book data to the template
+            // Using extract() here for simplicity within the loop to make $book available directly
+            extract(['book' => $book]);
+            include __DIR__ . '/../../components/template/book_card.template.php';
+            ?>
+        <?php endforeach; ?>
+    </div>
+</section>
+
+<?php
+require_once __DIR__ . '/../../components/componentGroup/footer.component.php';
+?>
