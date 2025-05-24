31ee938c8c52b124ba435164ead9df7bf26dd9be
Buenaventura123 <202310536@fit.edu.ph>
2025-05-24 10:26:11 +0800

    feat: created a cart page

--- page/cart/index.php
+++ page/cart/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); // Start the session to manage cart
 $pageTitle = 'Shopping Cart';
-$pageCss = '/AD-Bookstore/page/cart/assets/css/cart.css';
+$pageCss = '/AD-Task-3/page/cart/assets/css/cart.css';
 require_once __DIR__ . '/../../components/componentGroup/header.component.php';
 require_once __DIR__ . '/../../utils/app_utils.php';
 
@@ -12,7 +12,7 @@ $total_price = 0;
 <section class="shopping-cart">
     <h2>Your Shopping Cart</h2>
     <?php if (empty($cart)): ?>
-        <p>Your cart is empty. <a href="/AD-Bookstore/page/home/index.php">Start shopping!</a></p>
+        <p>Your cart is empty. <a href="/AD-Task-3/page/home/index.php">Start shopping!</a></p>
     <?php else: ?>
         <div class="cart-items">
             <?php foreach ($cart as $book_id => $quantity):
@@ -29,7 +29,7 @@ $total_price = 0;
                             <p>Quantity: <?php echo $quantity; ?></p>
                             <p>Subtotal: $<?php echo number_format($item_price, 2); ?></p>
                         </div>
-                        <form action="/AD-Bookstore/handlers/book_operations.handler.php" method="post" class="remove-form">
+                        <form action="/AD-Task-3/handlers/book_operations.handler.php" method="post" class="remove-form">
                             <input type="hidden" name="action" value="remove_from_cart">
                             <input type="hidden" name="book_id" value="<?php echo htmlspecialchars($book['id']); ?>">
                             <button type="submit" class="btn btn-remove">Remove</button>
@@ -41,7 +41,7 @@ $total_price = 0;
         </div>
         <div class="cart-summary">
             <h3>Total: $<?php echo number_format($total_price, 2); ?></h3>
-            <form action="/AD-Bookstore/handlers/book_operations.handler.php" method="post">
+            <form action="/AD-Task-3/handlers/book_operations.handler.php" method="post">
                 <input type="hidden" name="action" value="clear_cart">
                 <button type="submit" class="btn btn-clear-cart">Clear Cart</button>
             </form>
