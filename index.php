96b837886c3c177939876054543f6028f78fc356
Buenaventura123 <202310536@fit.edu.ph>
2025-05-24 15:36:44 +0800

    style: removed white spaces

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to AD Bookstore</title>
+    <title>Not A Bookstore</title>
     <style>
         body {
             font-family: Arial, sans-serif;
